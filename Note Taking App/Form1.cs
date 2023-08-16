bc0d4b27e89755e5db00be55063e18ddf3e54f2f
Kristoffermg <33570390+Kristoffermg@users.noreply.github.com>
2023-08-17 01:30:22 +0200

    Lots of implementations
    -> set up the structure for saving child notes
    -> set up function for getting a single integer value from a query (relevant for orderid and headerID so far)
    -> added some other minor stuff

--- Note Taking App/Form1.cs
+++ Note Taking App/Form1.cs
@@ -16,12 +16,16 @@ namespace Note_Taking_App
 {
     public partial class Form1 : Form
     {
+        const int MAX_TITLE_CHARACTER_LENGTH = 25;
+
         string connectionString = "Server=krishusdata.mysql.database.azure.com;Port=3306;database=NoteTakingApp;user id=kmg;password=krissupersecretpassword0!";
         bool headerNotesAreDisplayed = true;
+        int currentHeaderNoteSelected = 0;
+        Dictionary<int, string> allChildNotes = new Dictionary<int, string>();
         DataTable notes = new DataTable();
         public Form1()
         {
-            // form DataTables for the notes
+            // TODO: form the allChildNotes dictionary
             InitializeComponent();
         }
 
@@ -43,6 +47,7 @@ namespace Note_Taking_App
 
         private void AddNote_Click(object sender, EventArgs e)
         {
+<<<<<<< Updated upstream
             IDataAccess dataAccess = new DataAccess();
             string query = "";
             if (headerNotesAreDisplayed)
@@ -57,6 +62,47 @@ namespace Note_Taking_App
             }
         }
 
+=======
+            if(addNoteTitle.Text.Length <= MAX_TITLE_CHARACTER_LENGTH)
+            {
+                string query = "";
+                int orderid = GetOrderId();
+                if (headerNotesAreDisplayed)
+                {
+                    query = $"INSERT INTO HeaderNotes (title, orderid) VALUES('{addNoteTitle.Text}', {orderid + 1})";
+                    dataAccess.InsertData<HeaderNotes, dynamic>(query, new { }, connectionString);
+                    notes.Rows.Add(addNoteTitle.Text);
+
+                    query = $"SELECT MAX(id) AS value FROM HeaderNotes";
+                    int headerID = dataAccess.LoadSingularDataValue(query, connectionString);
+
+                    query = $"INSERT INTO ChildNotes (headerID, title, content, orderid) VALUES({headerID}, 'New Note', '', {headerID})"; // TODO: change orderid value
+                    dataAccess.InsertData<HeaderNotes, dynamic>(query, new { }, connectionString);
+                    
+                    // TODO: store the new data in the allChildNotes dictionary
+                }
+                else
+                {
+                    dataAccess.InsertData<ChildNotes, dynamic>(query, new { }, connectionString);
+                }
+            }
+        }
+
+        private int GetOrderId()
+        {
+            int orderid = 1;
+            if(headerNotesAreDisplayed)
+            {
+                orderid = dataAccess.LoadSingularDataValue("SELECT MAX(orderid) AS value FROM HeaderNotes", connectionString);
+            }
+            else
+            {
+                orderid = dataAccess.LoadSingularDataValue("SELECT MAX(orderid) AS value FROM ChildNotes", connectionString);
+            }
+            return orderid;
+        }
+
+>>>>>>> Stashed changes
         private void addNoteTitle_KeyDown(object sender, KeyEventArgs e)
         {
             if(e.KeyCode == Keys.Enter && addNoteTitle.Text != string.Empty)
@@ -66,5 +112,45 @@ namespace Note_Taking_App
                 e.SuppressKeyPress = true;
             }
         }
+<<<<<<< Updated upstream
+=======
+
+        private void SortBy_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedSortBy = SortBy.Text;
+            switch(selectedSortBy)
+            {
+                case "Custom":
+                    break;
+                case "Alphabetically":
+                    break;
+            }
+        }
+
+        private void HeaderNotes_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int test = e.RowIndex;
+
+            
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            SaveCurrentNote();
+        }
+
+        private void NoteInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if((ModifierKeys & Keys.Control) == Keys.Control && e.KeyCode == Keys.S)
+            {
+                SaveCurrentNote();
+            }
+        }
+
+        private void SaveCurrentNote()
+        {
+            // TODO: also add integration for when you switch to another note so it always saves
+        }
+>>>>>>> Stashed changes
     }
 }
